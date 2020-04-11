6f083b809f2dd9be8c9faafb2a4e0e8e1031a9f2
Anthony Le Goas <anthony.legoas@gmail.com>
2020-04-11 20:31:42 +0200

    ✨ (common) add <SocialLinks>

---
+++ src/common/social/SocialLinks/SocialLinks.svelte
@@ -0,0 +1,21 @@
+<script>
+  import IconLink from "../../../cdk/icons/IconLink/IconLink.svelte";
+
+  const size = 24;
+</script>
+
+<style>
+  .social-links {
+    display: flex;
+    align-items: center;
+  }
+</style>
+
+<div class="social-links">
+  <IconLink
+    href="https://www.meetup.com/fr-FR/UX-Design-Brest/"
+    name="meetup"
+    {size} />
+  <IconLink href="https://twitter.com/UXDesignBrest" name="twitter" {size} />
+  <IconLink href="https://github.com/UXDesignBrest" name="github" {size} />
+</div>
