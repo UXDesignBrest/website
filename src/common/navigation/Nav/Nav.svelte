fc4bd54f58711e38c607bcfdd0100fbf41307e4d
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-02 20:24:48 +0200

    🎨 (common) use <Menu> in <Nav>

--- src/common/navigation/Nav/Nav.svelte
+++ src/common/navigation/Nav/Nav.svelte
@@ -1,13 +1,13 @@
 <script>
   import IconLink from "../../../cdk/icons/IconLink/IconLink.svelte";
   import SocialLinks from "../../social/SocialLinks/SocialLinks.svelte";
+  import Menu from "./Menu/Menu.svelte";
   export let segment;
 </script>
 
 <style>
   nav {
     display: flex;
-    justify-content: space-between;
     align-items: center;
     font-weight: 300;
     padding: 1em;
@@ -17,59 +17,6 @@
   .logo {
     max-height: 4em;
     max-width: 15em;
-  }
-
-  ul {
-    margin: 0 4em 0 0;
-    padding: 0;
-  }
-
-  /* clearfix */
-  ul::after {
-    content: "";
-    display: block;
-    clear: both;
-  }
-
-  li {
-    display: block;
-    float: left;
-    margin: 0 1em;
-  }
-
-  a {
-    text-decoration: none;
-    padding: 1em 0.5em;
-    display: block;
-    font-weight: bold;
-    position: relative;
-    display: inline-block;
-  }
-
-  a::before {
-    position: absolute;
-    z-index: -1;
-    opacity: 0;
-    content: "";
-    left: 0;
-    right: 100%;
-    transition: all 0.275s;
-  }
-
-  [aria-current] {
-    position: relative;
-    display: inline-block;
-  }
-
-  a:hover::before,
-  [aria-current]::before {
-    opacity: 1;
-    left: 0%;
-    right: 0%;
-    top: 55%;
-    bottom: 20%;
-    background: var(--secondary-color);
-    transform: skew(-20deg) scale(1) rotate(1deg);
   }
 
   .nav-links {
@@ -87,26 +34,7 @@
       src="uxdesignbrest/logo-purple.png" />
   </a>
   <div class="nav-links">
-    <ul>
-      <li>
-        <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-          Accueil
-        </a>
-      </li>
-      <li>
-        <a
-          rel="prefetch"
-          aria-current={segment === 'blog' ? 'page' : undefined}
-          href="blog">
-          Evènements
-        </a>
-      </li>
-      <li>
-        <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-          À propos
-        </a>
-      </li>
-    </ul>
+    <Menu {segment} />
     <SocialLinks />
   </div>
 </nav>
