b5c0d086ac600ec45452a5db2f2899063a254438
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-03 18:46:37 +0200

    ✨ (routes) add route 'events'

---
+++ src/routes/events/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  import { useMeetupEvents } from "../../api";
+  export function preload({ params, query }) {
+    const { getNextEvent, getPastEvents } = useMeetupEvents(this);
+    return Promise.all([getNextEvent(), getPastEvents()]).then(values => ({
+      events: [...values[0], ...values[1]]
+    }));
+  }
+</script>
+
+<script>
+  import Events from "../../common/events/Events/Events.svelte";
+  export let events;
+</script>
+
+<svelte:head>
+  <title>Evénements</title>
+</svelte:head>
+
+<Events {events} />
