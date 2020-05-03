590111836b85d43ddf41f74c634052bc52508291
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-03 17:01:20 +0200

    ✨ (cdk) add <LogoAnimated>

---
+++ src/cdk/logos/LogoAnimated/LogoAnimated.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { fade, fly } from "svelte/transition";
+  import { onMount } from "svelte";
+
+  let visible = false;
+
+  onMount(async () => {
+    visible = true;
+  });
+</script>
+
+<style>
+  .logo-animated {
+    font-size: 2.5rem;
+    text-decoration: none;
+    margin: 0 1em;
+  }
+</style>
+
+{#if visible}
+  <a class="logo-animated" href="/" aria-label="Logo UX Design Brest">
+    {#each 'UX' as a, i}
+      <span transition:fly={{ delay: 100 + i * 20, duration: 800 }}>{a}</span>
+    {/each}
+    {#each 'Design' as b, i}
+      <span transition:fly={{ delay: 200 + i * 20, duration: 800 }}>{b}</span>
+    {/each}
+    {#each 'Brest' as c, i}
+      <span transition:fly={{ delay: 300 + i * 20, duration: 800 }}>{c}</span>
+    {/each}
+  </a>
+{/if}
