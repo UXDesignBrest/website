044e5687b737c98fa8c93ad32b1949de4271ca00
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-08 18:05:22 +0200

    💬 (routes) update routes title

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>About</title>
+  <title>À propos • UX Design Brest</title>
 </svelte:head>
 
 <h1>About this site</h1>
--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <svelte:head>
-  <title>Evénements</title>
+  <title>Evénements • UX Design Brest</title>
 </svelte:head>
 
 <Events {events} />
