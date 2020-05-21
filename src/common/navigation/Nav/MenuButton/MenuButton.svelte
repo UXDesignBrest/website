f8c6cccf3726ef359d536205df96b69e7811e1f0
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-21 18:40:15 +0200

    ✨ (common) add <MenuButton>

---
+++ src/common/navigation/Nav/MenuButton/MenuButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { menuStore } from "../../../../stores";
+  import IconButton from "../../../../cdk/icons/IconButton/IconButton.svelte";
+
+  const { menuIsOpen, toggleMenu } = menuStore;
+</script>
+
+<style>
+  :global(.hide-if-desktop) {
+    display: none !important;
+  }
+
+  @media only screen and (max-width: 840px) {
+    :global(.hide-if-desktop) {
+      display: block !important;
+    }
+  }
+</style>
+
+<IconButton
+  class="hide-if-desktop {$$props.class}"
+  name={$menuIsOpen ? 'close' : 'menu'}
+  on:click={() => toggleMenu()} />
