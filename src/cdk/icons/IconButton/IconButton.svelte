6392433163e39e7448ed600947d9a26733cebb27
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-21 18:34:06 +0200

    ✨ (cdk) add <IconButton>

---
+++ src/cdk/icons/IconButton/IconButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Icon from "../Icon/Icon.svelte";
+  export let name;
+  export let size = 20;
+</script>
+
+<style>
+  .icon-button {
+    border: none;
+    background: transparent;
+  }
+</style>
+
+<button class="icon-button {$$props.class}" on:click>
+  <Icon {name} {size} />
+</button>
