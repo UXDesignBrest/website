e4a5aaf9c727f8616a91b909596f3f153e2d7119
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-21 18:43:04 +0200

    ♻ (common) refacto <Menu> to make it responsive

--- src/common/navigation/Nav/Menu/Menu.svelte
+++ src/common/navigation/Nav/Menu/Menu.svelte
@@ -1,8 +1,15 @@
 <script>
+  import SocialLinks from "../../../social/SocialLinks/SocialLinks.svelte";
+
   export let segment;
+  export let open = false;
 </script>
 
 <style>
+  .menu {
+    display: flex;
+    align-items: center;
+  }
   ul {
     margin: 0 4em 0 0;
     padding: 0;
@@ -55,25 +62,91 @@
     background: var(--secondary-color);
     transform: skew(-20deg) scale(1) rotate(1deg);
   }
+
+  @media (max-width: 1100px) {
+    ul {
+      margin: 0 1em 0 0;
+    }
+  }
+
+  @media (max-width: 840px) {
+    .menu {
+      padding: 0 3rem 0 0;
+      position: fixed;
+      top: 0;
+      height: 0;
+      width: 100%;
+      transition: all 0.2s;
+    }
+
+    .menu.open {
+      list-style: none;
+      padding: 0 0 1em 0;
+      background: rgba(255, 255, 255, 0.95);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      z-index: 1;
+      height: 100%;
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      line-height: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    li {
+      display: none;
+    }
+
+    .menu.open ul li {
+      font-size: 1.1rem;
+      display: block;
+    }
+
+    .menu .social-links {
+      display: none;
+    }
+
+    .menu.open .social-links {
+      display: block;
+      margin: 2em 0;
+    }
+  }
 </style>
 
-<ul>
-  <li>
-    <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-      Accueil
-    </a>
-  </li>
-  <li>
-    <a
-      rel="prefetch"
-      aria-current={segment === 'events' ? 'page' : undefined}
-      href="events">
-      Evénements
-    </a>
-  </li>
-  <li>
-    <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-      À propos
-    </a>
-  </li>
-</ul>
+<div class="menu" class:open>
+  <ul>
+    <li>
+      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+        Accueil
+      </a>
+    </li>
+    <li>
+      <a
+        rel="prefetch"
+        aria-current={segment === 'events' ? 'page' : undefined}
+        href="events">
+        Evénements
+      </a>
+    </li>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
+        À propos
+      </a>
+    </li>
+  </ul>
+  <div class="social-links">
+    <SocialLinks />
+  </div>
+</div>
---
+++ src/common/navigation/Nav/Menu/MenuContainer.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Menu from "./Menu.svelte";
+  import { menuStore } from "../../../../stores";
+  import { stores } from "@sapper/app";
+
+  export let segment;
+
+  const { closeMenu, menuIsOpen } = menuStore;
+  const { page } = stores();
+
+  // Close the menu when the page changes.
+  page.subscribe(() => closeMenu());
+</script>
+
+<Menu {segment} open={$menuIsOpen} />
